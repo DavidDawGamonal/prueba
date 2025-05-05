46dc6c7d755a5e142e667e6a0d1b481b7bdc73d0
david.larman.1 <david.larman.1@educa.jcyl.es>
2025-05-05 10:28:46 +0200

    hola

--- PruebaGitHub.java
+++ PruebaGitHub.java
@@ -3,7 +3,8 @@ package pruebaRepositorioPack;
 public class PruebaGitHub {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hola tio");
 
 	}
 
